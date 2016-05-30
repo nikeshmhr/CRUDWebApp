96b86cbf56f3e58275e8aa0fe3d6f97fc291beaf
Nikesh Maharjan <nikeshmhr@gmail.com>
2016-05-30 11:56:49 +0545

    final commit before integrating angular

--- src/main/java/com/crudwebapp/config/DatabaseConfig.java
+++ src/main/java/com/crudwebapp/config/DatabaseConfig.java
@@ -1,14 +1,15 @@
 package com.crudwebapp.config;
 
 import java.io.IOException;
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -19,20 +20,21 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
  */
 @Configuration
 @ComponentScan(basePackages = "com.crudwebapp.config")
-@PropertySource(value = "classpath:credentials.properties", ignoreResourceNotFound = true)
+@PropertySource(value = "classpath:credentials.properties")
 public class DatabaseConfig {
 
-    private final String HOST = "jdbc:sqlserver://10.13.212.13:1433;databaseName=crud_test_nikesh";
-    private final String USER = "fonebank";
-    private final String PASS = "fonebank";
+//    private final String HOST = "jdbc:sqlserver://10.13.212.13:1433;databaseName=crud_test_nikesh";
+//    private final String USER = "fonebank";
+//    private final String PASS = "fonebank";
 
-    @Value("${jdbc.host}")
-    private String host;
+    @Resource
+    private Environment env;
 
     @Bean
     public DataSource dataSource() throws IOException {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource(HOST, USER, PASS);
-        dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        System.out.println("HOST " + env.getProperty("jdbc.host"));
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(env.getProperty("jdbc.host"), env.getProperty("jdbc.username"), env.getProperty("jdbc.password"));
+        dataSource.setDriverClassName(env.getProperty("jdbc.driver"));
         return dataSource;
     }
 
